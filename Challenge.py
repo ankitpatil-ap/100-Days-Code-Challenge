585b8184b83d360a898df7f4f4de907c7cddaaea
Ankit Patil <ankitpatil.v@gmail.com>
2023-09-15 23:50:16 +0530

    Day 1

---
+++ Challenge.py
@@ -0,0 +1,2 @@
+print("This is 100 Days of Code Challenge which begins from Now on behalf of Engineers Days.")
+print("I am taking this challenge and will coding consistently for 100 days from today itself.")
