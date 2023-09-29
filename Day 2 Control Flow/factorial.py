61c9bd168b32de7f6d738984c7ecb38af150faa4
Ankit Patil <ankitpatil.v@gmail.com>
2023-09-29 23:10:44 +0530

    Day 11 Still working on basics of control statements.

--- Day 2 Control Flow/factorial.py
+++ Day 2 Control Flow/factorial.py
@@ -1,4 +1,7 @@
 #: Create a program that calculates the factorial of a number entered by the user.
 import math
 num = int(input("Enter the number: "))
-fact = num!
+for i in num:
+    result = num * (num-i)
+    sum = num = i
+print(sum)
