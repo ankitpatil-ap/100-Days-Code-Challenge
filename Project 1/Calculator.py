d7612fa791b07286b1039d4460c71580ac18c38a
Ankit Patil <ankitpatil.v@gmail.com>
2023-10-09 23:54:38 +0530

    Modulo Operator

--- Project 1/Calculator.py
+++ Project 1/Calculator.py
@@ -1,1 +1,17 @@
 #Create a basic calculator program.
+a = int(input())
+b = int(input())
+c = input()
+
+if c == '+':
+    print(a+b)
+elif c == '-':
+    print(a-b)
+elif c == '*':
+    print(a*b)
+elif c == '/':
+    print(a/b)
+elif c == '%':
+    print(a%b)
+else:
+    print("Invalid Number")
