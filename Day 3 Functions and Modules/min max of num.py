88b2f8c3c985f6ec05ce9ff867efbf1dea8b0001
Ankit Patil <ankitpatil.v@gmail.com>
2023-10-10 23:27:15 +0530

    Functions Tricks

--- Day 3 Functions and Modules/min max of num.py
+++ Day 3 Functions and Modules/min max of num.py
@@ -1,4 +1,5 @@
 #Define functions to find the maximum and minimum of three numbers
+# https://www.w3schools.com/python/python_functions.asp
 
 n = input("Enter first Number: ")
 m = input("Enter first Number: ")
