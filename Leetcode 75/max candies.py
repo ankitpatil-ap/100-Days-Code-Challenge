a0eac07a18c70d61395c146008c4b7dcab21c85b
Ankit Patil <ankitpatil.v@gmail.com>
2023-10-31 23:58:09 +0530

    100

---
+++ Leetcode 75/max candies.py
@@ -0,0 +1,15 @@
+kids = int(input())
+candies = [int(input()) for _ in range(kids)]
+#print(candies)
+result = []
+
+maxi = max(candies)
+extraCandies = int(input())
+
+for kidscandies in candies:
+    if kidscandies + extraCandies>=maxi:
+        result.append(True)
+    else: 
+        result.append(False)
+    
+print(result)#
